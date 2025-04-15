dcc49a79456c4f0f127a6b494a660778cbaceeca
RadwaSamirr <radwa.samir@jumia.com>
2025-04-15 10:40:11 +0200

    Add files via upload

---
+++ my-webpage/script.js
@@ -0,0 +1,5 @@
+function sayHello() {
+    let message = document.getElementById("message");
+    message.innerText = "Hello,Docker!";
+    message.style.color = "blue"; // Set text color to pink
+}
