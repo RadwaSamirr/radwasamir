66dc1a6ac08a7a692807f52e42f9a4187a141273
RadwaSamirr <radwa.samir@jumia.com>
2025-04-17 16:07:24 +0200

    Update app.py

--- insert-update/app.py
+++ insert-update/app.py
@@ -86,4 +86,4 @@ def health_check():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=2222, debug=True)
